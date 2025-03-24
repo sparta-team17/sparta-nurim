276aa0abb67735504be8098cc7e917d5552b9a68
friedSaewoo <zriag@naver.com>
2025-03-24 18:47:04 +0900

    first commit

---
+++ src/test/java/com/example/nurim/NurimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
