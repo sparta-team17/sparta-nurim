2d31400dc7e7db870ddb6abe577b7f7cba0fa392
friedSaewoo <zriag@naver.com>
2025-03-26 02:43:02 +0900

    Test: 공지사항 서비스 테스트코드 추가

--- src/main/java/com/example/nurim/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/nurim/domain/notice/controller/NoticeController.java
@@ -7,9 +7,9 @@ import com.example.nurim.domain.notice.dto.response.NoticeSearchResponseDto;
 import com.example.nurim.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,22 +18,23 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping("/admin/notices")
-    public ResponseEntity<NoticeResponseDto> createNotice(@RequestBody NoticeRequestDto requestDto){
-        Long userId = 1L;
-        return new ResponseEntity<>(noticeService.createNotice(userId, requestDto.getTitle(), requestDto.getContents()), HttpStatus.CREATED);
+    public ResponseEntity<NoticeResponseDto> createNotice(@RequestBody NoticeRequestDto requestDto,
+                                                          @AuthenticationPrincipal AuthUser authUser
+                                                          ){
+        return new ResponseEntity<>(noticeService.createNotice(authUser.getId(), requestDto.getTitle(), requestDto.getContents()), HttpStatus.CREATED);
     }
 
     @PatchMapping("/admin/notices/{noticeId}")
     public ResponseEntity<NoticeResponseDto> updateNotice(@PathVariable Long noticeId,
-                                                          @RequestBody NoticeRequestDto requestDto){
-        Long userId = 1L;
-        return ResponseEntity.ok(noticeService.updateNotice(userId, noticeId, requestDto.getTitle(), requestDto.getContents()));
+                                                          @RequestBody NoticeRequestDto requestDto,
+                                                          @AuthenticationPrincipal AuthUser authUser){
+        return ResponseEntity.ok(noticeService.updateNotice(authUser.getId(), noticeId, requestDto.getTitle(), requestDto.getContents()));
     }
 
     @DeleteMapping("/admin/notices/{noticeId}")
-    public ResponseEntity<NoticeResponseDto> deleteNotice(@PathVariable Long noticeId){
-        Long userId = 1L;
-        return ResponseEntity.ok(noticeService.deleteNotice(userId, noticeId));
+    public ResponseEntity<NoticeResponseDto> deleteNotice(@PathVariable Long noticeId,
+                                                          @AuthenticationPrincipal AuthUser authUser){
+        return ResponseEntity.ok(noticeService.deleteNotice(authUser.getId(), noticeId));
     }
 
     @GetMapping("/notices/{noticeId}")
