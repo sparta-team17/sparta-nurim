830eecdaf641884956ac95fb0f0f73fa219801e2
iboyeon0816 <iboyeon0816@gmail.com>
2025-03-25 10:13:58 +0900

    Feat: 인증 필터 구현

--- src/main/java/com/example/nurim/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/nurim/config/GlobalExceptionHandler.java
@@ -4,19 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message){
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("status,", status.name());
-        errorResponse.put("code", status.value());
-        errorResponse.put("message", message);
-
+        Map<String, Object> errorResponse = ErrorResponseUtil.getErrorResponse(status, message);
         return new ResponseEntity<>(errorResponse, status);
-
     }
 }
