ab4e8308b037b88912506d29d47fc24b9a43dcae
friedSaewoo <zriag@naver.com>
2025-03-26 10:49:04 +0900

    Feat: 예외처리 추가

--- src/main/java/com/example/nurim/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/nurim/config/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.nurim.config;
 
 import com.example.nurim.domain.auth.exception.AuthException;
+import com.example.nurim.domain.common.exception.InvalidRequestException;
+import com.example.nurim.domain.common.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +16,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<Map<String, Object>> handleAuthException(AuthException e) {
         return getErrorResponse(e.getStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<Map<String, Object>> invalidRequestException(InvalidRequestException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getErrorResponse(status, ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Map<String, Object>> unauthorizedException(UnauthorizedException ex) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        return getErrorResponse(status, ex.getMessage());
     }
 
     public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
